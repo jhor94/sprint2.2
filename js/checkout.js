4ad4fa8f06d00bf9b48a09bee10a95a0a277c45e
Jhorman Cadena Valero <jhor@MacBook-Air-de-Jhorman.local>
2024-07-29 17:42:37 +0200

    sprint 2.1 actualizado corregido error de validacion de formulario

--- js/checkout.js
+++ js/checkout.js
@@ -1,128 +1,112 @@
 
 // Exercise 6
-function validate() {
-	var error = 0;
-	// Get the input fields
-	let form = document.getElementById("form1")
-	var fName = document.getElementById("fName");
-	var fLastName = document.getElementById("fLastN");
-	var fEmail = document.getElementById("fEmail");
-	var fPassword = document.getElementById("fPassword");
-	var fPhone = document.getElementById("fPhone");
-	var fAddress = document.getElementById("fAddress");
+// Get the input fields
+var fName = document.getElementById("fName");
+var fLastName = document.getElementById("fLastN");
+var fEmail = document.getElementById("fEmail");
+var fPassword = document.getElementById("fPassword");
+var fPhone = document.getElementById("fPhone");
+var fAddress = document.getElementById("fAddress");
 
-	// Get the error elements
-	var errorName = document.getElementById("errorName");
-	var errorLastName = document.getElementById("errorLastN");  
-	var errorEmail = document.getElementById("errorEmail");  
-	var errorPassword = document.getElementById("errorPassword"); 
-	var errorPhone = document.getElementById("errorPhone");
-	var errorAddress = document.getElementById("errorAddress");
+
+function comprobarLetra(palabra) {
+	let esTodoLetra = /^[A-Za-zÀ-ÿ\s]+$/.test(palabra)// expresion regular que indica si es todo letra
+	return esTodoLetra
+}
+
+function comprobarNumero(telNumero) {
+	let esNumero = /^[0-9]+$/.test(telNumero)
+	return esNumero
+}
+
+function comprobarEmail(textEmail) {
+	let esEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(textEmail)
+	return esEmail
+}
+
+function comprobarPassword(textPassword) {
+	let esPasword = /^[a-zA-Z0-9]{1,}$/.test(textPassword)
+	return esPasword
+}
+
+
+function validate(event) {
+	event.preventDefault();
+
 	
 	// Validate fields entered by the user: name, phone, password, and email
 
-	form.addEventListener('submit',(e)=>{
-		e.preventDefault()
-		validarCampos()
+	if (fName.value.trim() === "" || fName.value.length < 3 || !comprobarLetra(fName.value)) {
+		fName.classList.add("is-invalid");
+	} else {
+		fName.classList.remove("is-invalid");
+		fName.classList.add("is-valid");
+	}
+
+	if (fLastName.value.trim() === "" || fLastName.value.length < 3 || !comprobarLetra(fLastName.value)) {
+		fLastName.classList.add("is-invalid");
+		error++;
+	} else {
+		fLastName.classList.remove("is-invalid");
+		fLastName.classList.add("is-valid");
 		
-	})
-
-	const comprobarLetra = (palabra) =>{
-		let esTodoLetra = /^[a-zA-ZÀ-ÿ\s]{1,}$/.test(palabra)// expresion regular que indica si es todo letra
-		return esTodoLetra
 	}
 
-	const comprobarNumero = (telNumero) => {
-		let esNumero = /^[0-9]+$/.test(telNumero)
-		return esNumero
+	if (fEmail.value.trim() === "" || fEmail.value.length < 3 || !comprobarEmail(fEmail.value)) {
+		fEmail.classList.add("is-invalid")
+	} else {
+		fEmail.classList.remove("is-invalid");
+		fEmail.classList.add("is-valid");
 	}
 
-	const comprobarEmail = (textEmail) => {
-		let esEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(textEmail)
-		return esEmail
+	if (fPassword.value.trim() === "" || fPassword.value.length < 3 || !comprobarPassword(fPassword.value)) {
+		fPassword.classList.add("is-invalid")
+	} else {
+		fPassword.classList.remove("is-invalid");
+		fPassword.classList.add("is-valid");
 	}
 
-	const comprobarPassword = (textPassword) => {
-		let esPasword = /^[a-zA-Z0-9]{1,}$/.test(textPassword)
-		return esPasword
+	if (fPhone.value.trim() === "" || fPhone.value.length < 3 || !comprobarNumero(fPhone.value)) {
+		fPhone.classList.add("is-invalid")
+	} else {
+		fPhone.classList.remove("is-invalid");
+		fPhone.classList.add("is-valid");
 	}
 
-	const validarCampos = () =>{
-	
-		if(fName.value.trim() === "" || fName.value.length < 3 || !comprobarLetra(fName.value)){
-			errorName.style.display = 'block'
-			error++;
-		}else{
-			errorName.style.display = 'none'
-		}
-
-		if(fLastName.value.trim() === ""|| fLastName.value.length < 3 || !comprobarLetra(fLastName.value) ){
-			errorLastName.style.display = 'block'
-			error++;
-		}else{
-			errorLastName.style.display = 'none'
-		}
-
-		if(fEmail.value.trim() === ""|| fEmail.value.length < 3 || !comprobarEmail(fEmail.value)){
-			errorEmail.style.display = 'block'
-			error++
-		}else{
-			errorEmail.style.display = 'none'
-		}
-
-		if(fPassword.value.trim() === ""|| fPassword.value.length < 3 || !comprobarPassword(fPassword.value)){
-			errorPassword.style.display = 'block'
-			error++
-		}else{
-			errorPassword.style.display = 'none'
-		}
-
-		if(fPhone.value.trim() === ""|| fPhone.value.length < 3 || !comprobarNumero(fPhone.value)){
-			errorPhone.style.display = 'block'
-			error++
-		}else{
-			errorPhone.style.display = 'none'
-		}
-
-		if(fAddress.value.trim() === ""|| fAddress.value.length < 3){
-			errorAddress.style.display = 'block'
-			error++
-		}else{
-			errorAddress.style.display = 'none'
-		}
+	if (fAddress.value.trim() === "" || fAddress.value.length < 3) {
+		fAddress.classList.add("is-invalid")
+		
+	} else {
+		fAddress.classList.remove("is-invalid");
+		fAddress.classList.add("is-valid");
 	}
-
-	// Example starter JavaScript for disabling form submissions if there are invalid fields
-	(function () {
-		'use strict'
-	  
-		// Fetch all the forms we want to apply custom Bootstrap validation styles to
-		var forms = document.querySelectorAll('.needs-validation')
-	  
-		// Loop over them and prevent submission
-		Array.prototype.slice.call(forms)
-		  .forEach(function (form) {
-			form.addEventListener('submit', function (event) {
-			  if (!form.checkValidity()) {
-				event.preventDefault()
-				event.stopPropagation()
-			  }
-	  
-			  form.classList.add('was-validated')
-			}, false)
-		  })
-	  })()
-
-	
-
-
-	
-	/*
-	if(error >0){
-		alert("Error");
-	}else{
-		alert("OK");
-	}*/
-
 }
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
+
+
+
+
+
+
+
+
 
